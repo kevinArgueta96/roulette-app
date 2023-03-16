9c5c9567c67bba8d002d5f0157842b23e232c89f
kjarguetab <kev200050@gmail.com>
2023-03-16 10:20:17 -0600

    Modify size of rigth colimns

--- src/App.vue
+++ src/App.vue
@@ -53,7 +53,7 @@ export default {
   data: () => {
     return {
       isVisbleWinImg: false,
-      isVisbleLooseImg: false,
+      isVisbleLooseImg: true,
       isVisibleConfetti: false,
       screenWidth: 0,
       screenHeight:0,
@@ -100,17 +100,6 @@ export default {
       "setTopPriceScheduleRangeA",
       "setTopPriceScheduleRangeB",
     ]),
-    obtenerHoraActual() {
-      const ahora = new Date();
-      const hora = ahora.getHours();
-      const minutos = ahora.getMinutes();
-
-      // Formatear hora en formato 24 horas
-      const hora24 = (hora < 10 ? "0" : "") + hora;
-      const minutos24 = (minutos < 10 ? "0" : "") + minutos;
-
-      return `${hora24}:${minutos24}`;
-    },
     spinRoulleteByEnter(event) {
       if (event.keyCode === 13 || event.keyCode === 32) {
         this.spin();
